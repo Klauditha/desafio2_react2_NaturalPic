e644b6b071a4d1433fd1dec55d9632d252792285
Claudia Villarroel <claudia.villarroel@live.com>
2023-10-28 00:03:04 -0300

    :hammer: Configuraciones para deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"https://klauditha.github.io/desafio2_react2_NaturalPic",
   plugins: [react()],
 })

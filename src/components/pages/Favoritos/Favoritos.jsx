128100afb1d7501a62a5a9448ed934175415905b
Claudia Villarroel <claudia.villarroel@live.com>
2023-10-28 23:54:48 -0300

    :zap: Funcionalidad likes y despliegue favoritos

--- src/components/pages/Favoritos/Favoritos.jsx
+++ src/components/pages/Favoritos/Favoritos.jsx
@@ -1,10 +1,21 @@
+import { useContext } from "react";
+import { Context } from "../../Context/Context";
+
 const Favoritos = () => {
+  const { favoritesPictures } = useContext(Context);
   return (
     <div>
-      <h1 className="text-green-500 text-center">Fotos Favoritas</h1>
-      <div className="p-3 gallery grid-columns-4"></div>
+      <h1 className="text-green-500 text-center text-4xl p-2">Fotos Favoritas</h1>
+      <div className="flex flex-wrap">
+        {favoritesPictures.map((picture) => (
+          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/5 xl:w-1/4 p-1 bg-white-500" key={picture.id}>
+            <img src={picture.src.tiny} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default Favoritos;
+

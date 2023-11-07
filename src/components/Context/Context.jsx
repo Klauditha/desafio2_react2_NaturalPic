7815a39b8426d92f67324411b7594b480ba6c5f9
Claudia Villarroel <claudia.villarroel@live.com>
2023-11-06 23:17:52 -0300

    :bug: Se aplica axios

--- src/components/Context/Context.jsx
+++ src/components/Context/Context.jsx
@@ -1,21 +1,21 @@
 /* eslint-disable react/prop-types */
 import { createContext, useState, useEffect } from "react";
-import { photos } from "../../photos.json";
 import axios from "axios";
+
 export const Context = createContext();
 
 export const ProviderContext = ({ children }) => {
   const [pictures, setPictures] = useState([]);
 
   const getData = async () => {
-    axios.get("/photos.json").then((res) => {
-      setPictures(res.data);
+    axios.get("photos.json").then((res) => {
+      setPictures(res.data.photos);
     });
   };
 
   useEffect(() => {
     getData();
-  });
+  }, []);
 
   return (
     <Context.Provider value={{ pictures, setPictures }}>

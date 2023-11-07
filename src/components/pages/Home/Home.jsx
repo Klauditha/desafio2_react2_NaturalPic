14fa37b538b576ddc61c9e7f3806e69c2d87e78f
Claudia Villarroel <claudia.villarroel@live.com>
2023-11-06 23:23:08 -0300

    :pencil: Instrucciones para usuario

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -16,6 +16,7 @@ const Home = () => {
   return (
     <div>
       <h1 className="text-green-500 text-center text-4xl pt-6">Natural Pic</h1>
+      <h3 className="text-center pt-2">Haz clic en el icono  🤍 para agregar o quitar a Favoritos</h3>
       <div className="flex flex-wrap" id="picturesContainer">
         {pictures.map((picture) => (
           <div className="p-1 bg-white-500 picture" key={picture.id}>
